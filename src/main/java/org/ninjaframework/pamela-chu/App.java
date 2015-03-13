071cd5711d8b832e833472460591c69e347c8357
Aris FAVIA <Fix@MacBook-Pro-de-Aris.local>
2015-03-13 11:48:01 +0100

    ajout de la première structure du fullstack ("ninja")

---
+++ src/main/java/org/ninjaframework/pamela-chu/App.java
@@ -0,0 +1,13 @@
+package org.ninjaframework.pamela-chu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
