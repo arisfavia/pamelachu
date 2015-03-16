fb4f73d1a1dd18268386919d5a82995a3e0a6f86
Fabrice Trebuchon <fabrice.trebuchon@epitech.eu>
2015-03-16 15:38:14 +0100

    archi models controllers

--- src/main/java/models/UserModel.java
+++ src/main/java/models/UserModel.java
@@ -1,3 +1,17 @@
 public class UserModel {
-    
+    public function createUser() {
+        
+    }
+    public function deleteUser() {
+        
+    }
+    public function connectUser() {
+        
+    }
+    public function modifUser() {
+        
+    }
+    public function getInfoUser() {
+        
+    }
 }
